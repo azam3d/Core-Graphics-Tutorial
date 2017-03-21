7da907e2930342a1f12b59378e112f9f4970359a
Muhammad Azam Bin Baderi <m.azam@Azams-MacBook.local>
2017-03-21 12:18:17 +0800

    Add water meter.

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        counterLabel.text = String(counterView.counter)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnPushButton(button: PushButtonView) {
+        if button.isAddButton {
+            counterView.counter += 1
+        } else {
+            if counterView.counter > 0 {
+                counterView.counter -= 1
+            }
+        }
+        counterLabel.text = String(counterView.counter)
+    }
 }
 
