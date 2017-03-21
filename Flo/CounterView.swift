386de48150d33be67ea5d5ea756d37c0f050b92b
Muhammad Azam Bin Baderi <m.azam@Azams-MacBook.local>
2017-03-21 12:19:17 +0800

    Add CounterView class.

---
+++ Flo/CounterView.swift
@@ -0,0 +1,73 @@
+//
+//  CounterView.swift
+//  Flo
+//
+//  Created by Muhammad Azam Bin Baderi on 3/15/17.
+//  Copyright © 2017 Muhammad Azam Bin Baderi. All rights reserved.
+//
+
+import UIKit
+
+let NoOfGlasses = 8
+let π = CGFloat(M_PI)
+
+@IBDesignable class CounterView: UIView {
+    
+    @IBInspectable var counter: Int = 5 {
+        didSet {
+            if counter <=  NoOfGlasses {
+                setNeedsDisplay()
+            }
+        }
+    }
+    @IBInspectable var outlineColor: UIColor = .blue
+    @IBInspectable var counterColor: UIColor = .orange
+    
+    override func draw(_ rect: 	CGRect) {
+        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
+        
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        
+        let arcWidth: CGFloat = 76
+        
+        let startAngle: CGFloat = 3 * π / 4
+        let endAngle: CGFloat = π / 4
+        
+        let path = UIBezierPath(arcCenter: center,
+                                radius: radius/2 - arcWidth/2,
+                                startAngle: startAngle,
+                                endAngle: endAngle,
+                                clockwise: true)
+        
+        path.lineWidth = arcWidth
+        counterColor.setStroke()
+        path.stroke()
+        
+        let angleDifference: CGFloat = 2 * π - startAngle + endAngle
+        
+        //then calculate the arc for each single glass
+        let arcLengthPerGlass = angleDifference / CGFloat(NoOfGlasses)
+        
+        //then multiply out by the actual glasses drunk
+        let outlineEndAngle = arcLengthPerGlass * CGFloat(counter) + startAngle
+        
+        let outlinePath = UIBezierPath(arcCenter: center,
+                                       radius: bounds.width/2 - 2.5,
+                                       startAngle: startAngle,
+                                       endAngle: outlineEndAngle,
+                                       clockwise: true)
+        
+        outlinePath.addArc(withCenter: center,
+                                     radius: bounds.width/2 - arcWidth + 2.5,
+                                     startAngle: outlineEndAngle,
+                                     endAngle: startAngle,
+                                     clockwise: false)
+        outlinePath.close()
+        
+        outlineColor.setStroke()
+        outlinePath.lineWidth = 5.0
+        outlinePath.stroke()
+    }
+}
+
+
